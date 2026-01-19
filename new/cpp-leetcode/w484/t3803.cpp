0ebbd6e1db7b7465d2e12f6f3143097fe8cc5b25
Elaine <garhlz257@163.com>
2026-01-19 17:34:40 +0800

    update

---
+++ new/cpp-leetcode/w484/t3803.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int residuePrefixes(string s) {
+        vector<int> nums(26,0);
+        for(int i = 0;i < s.size();i++){
+            int c = s[i] - 'a';
+            nums
+        }
+    }
+};
